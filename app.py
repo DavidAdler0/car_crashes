8f983b0b91a7c77ffa8795b5bb3e1faa14160460
DavidAdler <adlerdovi@gmail.com>
2024-10-10 17:01:17 +0300

    added repositories and bluprints for tree first end points

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
+from blueprints.crashes_bp import crashes_blueprint
 from blueprints.init_bp import init_blueprint
 
 
 app = Flask(__name__)
 
-# app.register_blueprint(car_blueprint, url_prefix='/cars')
+app.register_blueprint(crashes_blueprint)
 app.register_blueprint(init_blueprint, url_prefix='/init')
 
 
